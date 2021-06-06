c8c623c6abe4fdf09e007d75e987dbdd96b1785f
kashelton17 <kashelton17@gmail.com>
2021-06-06 14:06:52 -0500

    adding fs and inquirer libaries

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
 const questions = ['Enter title of Application:', 'Enter a description:', 'Enter Table of Contents:', 'Enter installation:', 'Enter Usage:', 'Enter contributors:', 'Enter Tests', 'Enter GitHub username:', 'Enter email:'];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('README.md successfully written!'))
+}
 
 // TODO: Create a function to initialize app
 function init() {}
