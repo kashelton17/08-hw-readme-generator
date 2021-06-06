8635b516a055d34a1d20c04b88f5bc55915d1ade
kashelton17 <kashelton17@gmail.com>
2021-06-06 14:44:28 -0500

    adding user input function

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
+
 // TODO: Create an array of questions for user input
-const questions = ['Enter title of Application:', 'Enter a description:', 'Enter Table of Contents:', 'Enter installation:', 'Enter Usage:', 'Enter contributors:', 'Enter Tests', 'Enter GitHub username:', 'Enter email:'];
+const questions = ['Enter title of Application:', 'Enter a description:', 'Enter Table of Contents:', 'Enter installation:', 'Enter Usage:', 'Enter contributors:', 'Enter Tests','Enter license:', 'Enter GitHub username:', 'Enter email:'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -11,7 +12,67 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0],
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[1],
+            },
+            {
+                type: 'input',
+                name: 'table',
+                message: questions[2],
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: questions[3],
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: questions[4],
+            },
+            {
+                type: 'input',
+                name: 'contributers',
+                message: questions[5],
+            },
+            {
+                type: 'input',
+                name: 'test',
+                message: questions[6],
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[7],
+                choices:['MIT', 'NPM', 'other'],
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: questions[8],
+                
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[9],
+            },
+    ])
+    .then((data) => {
+        const userInput = data
+        console.log(userInput)
+    })
+}
 
 // Function call to initialize app
 init();
