b4e0d6fc624524f95ac42f0ed02b7c3b243c8ba2
kashelton17 <kashelton17@gmail.com>
2021-06-06 15:38:17 -0500

    adding license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT'){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (license === '') {
+    return ''
+  } else {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)]`
+  } 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT'
+  } else if (license === ''){
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+  const link = renderLicenseLink(license)
+  const licenseSection = `## License information \n This application uses a license from ${license} \n Link: ${link}`
+  return licenseSection
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const badge = renderLicenseBadge(license)
+  const licensePart = renderLicenseSection(license)
   return `# ${data.title}
 
 `;
